eba7ff37738326c8cacc1d51a3a2662cb3de5a5b
sahil sameer siddique <sahilsameer.dev18@gmail.com>
2026-04-12 21:38:39 +0530

    change client domain

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: "https://prepstack-liard.vercel.app",
+  origin: "https://prepstack-ss.vercel.app",
   credentials: true,
 }))
 
